8b27654596dc62edbe9de0c0df2a6c0cdb0bd539
Lan Chen <chenlaneva@mails.cuc.edu.cn>
2025-11-16 16:19:45 +0800

    Update inference.sh

--- inference.sh
+++ inference.sh
@@ -3,7 +3,7 @@ python inference.py \
   --model_dir "models" \
   --model_name "movement" \
   --output_path "output" \
-  --video_path  "test/camera_movement/0001.mp4" \
+  --video_path  "test/camera_movement/0002.mp4" \
   --prompt="[clip1] is the original source video, and [clip2] is its corresponding scribble map. [clip3] is another, different source video. In [clip4], the scribble map transformation applied from [clip1] to [clip2] is similarly applied to [clip3]." \
   --width 832 \
   --height 480 \
